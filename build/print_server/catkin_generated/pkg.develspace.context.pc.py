7250eb1418486d5fbf9d2310825b25c16f01641e
atussak <atussak@stud.ntnu.no>
2019-07-09 17:11:09 +0200

    add deps

--- build/print_server/catkin_generated/pkg.develspace.context.pc.py
+++ build/print_server/catkin_generated/pkg.develspace.context.pc.py
@@ -1,7 +1,7 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/user/Desktop/master/devel/include".split(';') if "/home/user/Desktop/master/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime".replace(';', ' ')
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime;actionlib_msgs".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
 PROJECT_NAME = "print_server"
 PROJECT_SPACE_DIR = "/home/user/Desktop/master/devel"
